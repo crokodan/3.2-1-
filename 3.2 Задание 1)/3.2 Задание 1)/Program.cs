58c3b9f6c48081eaeedfd3a42b79cdb957d9e953
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 18:46:08 +0400

    Add files via upload

---
+++ 3.2 Задание 1)/3.2 Задание 1)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._2_Задание_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i <= 100; i++)
+                if (i % 4 == 0) sum += i;
+            Console.WriteLine(sum);
+        }
+    }
+}
